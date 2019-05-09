5a28d1d604f6481a2f5cfca224f305682a531996
Guilherme Souza <guilherme.agsouza@hotmail.com>
2019-05-09 20:45:45 -0300

    Update colab3_convolutions.py

--- colab3_convolutions.py
+++ colab3_convolutions.py
@@ -94,10 +94,10 @@ test_images=test_images/255.0
 
 #Next is to define your model. Now instead of the input layer at the top, you're going to add a Convolution. The parameters are:
 
-1. The number of convolutions you want to generate. Purely arbitrary, but good to start with something in the order of 32
-2. The size of the Convolution, in this case a 3x3 grid
-3. The activation function to use -- in this case we'll use relu, which you might recall is the equivalent of returning x when x>0, else returning 0
-4. In the first layer, the shape of the input data.
+#1. The number of convolutions you want to generate. Purely arbitrary, but good to start with something in the order of 32
+#2. The size of the Convolution, in this case a 3x3 grid
+#3. The activation function to use -- in this case we'll use relu, which you might recall is the equivalent of returning x when x>0, else returning 0
+#4. In the first layer, the shape of the input data.
 
 #You'll follow the Convolution with a MaxPooling layer which is then designed to compress the image, while maintaining the content of the features that were highlighted by the convlution. By specifying (2,2) for the MaxPooling, the effect is to quarter the size of the image. Without going into too much detail here, the idea is that it creates a 2x2 array of pixels, and picks the biggest one, thus turning 4 pixels into 1. It repeats this across the image, and in so doing halves the number of horizontal, and halves the number of vertical pixels, effectively reducing the image by 25%.
 
